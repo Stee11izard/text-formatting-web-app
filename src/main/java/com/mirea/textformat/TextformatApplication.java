e8a6f8e4ae34a82216c9ab9482fc4f8fc179a20d
Egor Sablev <Steel.Lizard@yandex.ru>
2024-04-01 22:26:48 +0300

    Spring Boot Initializr

---
+++ src/main/java/com/mirea/textformat/TextformatApplication.java
@@ -0,0 +1,13 @@
+package com.mirea.textformat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextformatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextformatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mirea/textformat/TextformatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirea.textformat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextformatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
